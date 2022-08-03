5a3105c80b048451967bddc8e6bf472f6baf3502
Liutongxueforgaogao <64537558+Liutongxueforgaogao@users.noreply.github.com>
2022-08-03 14:03:48 +0800

    测试

---
+++ src/effectivejava/test/test1.java
@@ -0,0 +1,14 @@
+package effectivejava.test;
+
+public class test1 {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        //long sum=0;//1s钟
+        Long sum=0L;//10.185s钟
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum+=i;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+}
